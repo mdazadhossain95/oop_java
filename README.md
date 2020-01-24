3c4a2277bc85ae8c3fc7d9865470c6d3d6f19dde
mdazadhossain95 <mdazadhossain95@gmail.com>
2020-01-24 23:52:44 +0800

    add new hallo.class file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # oop_java
+echo "# oop_java" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mdazadhossain95/oop_java.git
+git push -u origin master
+
+git remote add origin https://github.com/mdazadhossain95/oop_java.git
+git push -u origin master
--- src/byteshortint.java
+++ src/byteshortint.java
@@ -1,2 +1,19 @@
 public class byteshortint {
-}
+
+        public static void main(String[] args) {
+
+            //int has a width of 32
+            int myminvalue= -2_147_483_648;
+            int mymaxvalue= 2_147_483_647;
+
+            //byte has a width of 8
+            byte mybytevalue = -128;
+
+            //short has a width of 16
+            short myshortvalue = 32767;
+
+            //long has a width of 64
+            long mylongvalue = 100L;
+
+        }
+    }
