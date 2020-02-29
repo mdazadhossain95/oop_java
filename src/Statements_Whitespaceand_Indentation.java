e6e83fc8f05bc483d373cddbeb373ca08e2a35cb
mdazadhossain95 <mdazadhossain95@gmail.com>
2020-03-01 04:43:05 +0600

    add new class

---
+++ src/Statements_Whitespaceand_Indentation.java
@@ -0,0 +1,24 @@
+public class Statements_Whitespaceand_Indentation {
+
+    public static void maim(String[] args) {
+
+        int myvariable = 50;
+//        if(myvariable == 50){
+//        System.out.println("print");
+//    }
+        myvariable++;
+        myvariable--;
+        System.out.println("this is a test");
+
+//        System.out.println("this is " +
+//                "anouther" +
+//                "case");
+//
+//        int anouthervariable = 50;
+//        myvariable--;
+//        System.out.println("this is a new case");
+
+
+
+    }
+}
