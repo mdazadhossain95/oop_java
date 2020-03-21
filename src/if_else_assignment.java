d305c125384c6365135e2cf4196838b841611eda
mdazadhossain95 <mdazadhossain95@gmail.com>
2020-03-21 15:32:26 +0600

    add new classes

--- src/if_else_assignment.java
+++ src/if_else_assignment.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class if_else_assignment {
 
-    public static void main (string args[]){
+    public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number.....");
         int num1 = scanner.nextInt();
